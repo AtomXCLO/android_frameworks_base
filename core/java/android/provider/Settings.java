a190fdbdd14cb9132c84ebef53ba4483fec393ad
Marko Man <darkobas@gmail.com>
2016-12-29 01:07:25 +0100

    base: Configure lock screen media artwork blur level [1/2]
    Change-Id: I68895ae6a471bbc99b538b85337e15e8793665ce

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5387,6 +5387,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
 
         /**
+          * media artwork wallpaper blur level on lockscreen
+          * @hide
+          */
+        public static final String LOCKSCREEN_MEDIA_BLUR = "lockscreen_media_blur";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
